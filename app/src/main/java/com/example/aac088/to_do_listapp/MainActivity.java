dbb060659ceb0af1ac5879bb08ab57f0ec184793
aac088 <albertoceballos20@gmail.com>
2017-09-19 00:44:18 -0500

    Fixed Delete Operation for list item

--- app/src/main/java/com/example/aac088/to_do_listapp/MainActivity.java
+++ app/src/main/java/com/example/aac088/to_do_listapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.aac088.to_do_listapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
@@ -29,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         txtview.setText(user_id);
 
-        Downloader d = new Downloader(this,addressMaster,listView,1,user_id,master_list_id);
-
+        Downloader3 d = new Downloader3();
         d.execute();
 
         imgview.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +65,173 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
             }
         });
+    }
+
+    public class Downloader3 extends AsyncTask<Void,Integer,String>{
+        private ProgressDialog pd;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setTitle("Downloading");
+            pd.setMessage("Downloading... Please wait");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String data=null;
+            data = downloadData();
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pd.dismiss();
+            if(s != null){
+                Parser3 p = new Parser3(s);
+                p.execute();
+            }else{
+                Toast.makeText(MainActivity.this,"Unable to download data",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public String downloadData(){
+            InputStream is = null;
+            String line = null;
+            String pass_data=null;
+
+            try {
+                pass_data = URLEncoder.encode("user_id","UTF-8")
+                        + "=" + URLEncoder.encode(user_id,"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                URL url = new URL(addressMaster);
+                HttpURLConnection con= (HttpURLConnection) url.openConnection();
+                con.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                if(pass_data != null){
+                    wr.write(pass_data);
+                    wr.flush();
+                }else{
+                    Toast.makeText(MainActivity.this,"Error passing data to server", Toast.LENGTH_SHORT).show();
+                }
+
+                is = new BufferedInputStream(con.getInputStream());
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                StringBuffer sb = new StringBuffer();
+
+                if(br != null){
+                    while((line=br.readLine()) != null){
+                        sb.append(line+"\n");
+                    }
+                }else{
+                    return null;
+                }
+
+                return sb.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                if(is != null){
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public class Parser3 extends AsyncTask<Void,Integer,Integer>{
+        private String data;
+        private ProgressDialog pd;
+        private ArrayList<String> list= new ArrayList<>();
+
+        Parser3(String data){
+            this.data=data;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setTitle("Parser");
+            pd.setMessage("Parsing... Please wait");
+            pd.show();
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            return parseData();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            pd.dismiss();
+
+            if(integer==1){
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,list);
+                listView.setAdapter(adapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        master_list_id=listView.getItemAtPosition(position).toString();
+                        Intent intent = new Intent(MainActivity.this,TaskActivity.class);
+                        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("master_list_id",master_list_id);
+                        editor.putString("user_id",user_id);
+                        editor.commit();
+                        MainActivity.this.startActivity(intent);
+
+                    }
+                });
+            }else{
+                Toast.makeText(MainActivity.this,"Unable to Parse",Toast.LENGTH_SHORT).show();
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("master_list_id",master_list_id);
+                editor.putString("user_id",user_id);
+                editor.commit();
+            }
+        }
+
+        public int parseData(){
+            try {
+                JSONArray ja = new JSONArray(data);
+                JSONObject jo = null;
+
+                list.clear();
+
+                for(int i=0;i<ja.length();i++){
+                    jo = ja.getJSONObject(i);
+
+                    String listname =jo.getString("listname");
+
+                    list.add(listname);
+                }
+
+                return 1;
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return 0;
+        }
     }
 }
--- app/src/main/java/com/example/aac088/to_do_listapp/TaskActivity.java
+++ app/src/main/java/com/example/aac088/to_do_listapp/TaskActivity.java
@@ -1,21 +1,44 @@
 package com.example.aac088.to_do_listapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class TaskActivity extends AppCompatActivity {
     private ListView listView;
@@ -24,7 +47,6 @@ public class TaskActivity extends AppCompatActivity {
     private String master_list_id,user_id;
     private String task_str;
     private String addressTask="https://albertoceballos20.000webhostapp.com/get_task.php";
-    private String cases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +56,11 @@ public class TaskActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.task_etxt);
         imageView = (ImageView) findViewById(R.id.add_task_img_vw);
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        master_list_id = preferences.getString("master_list_id",null);
+        user_id = preferences.getString("user_id",null);
 
-        cases = prefs.getString("cases", "0");
-
-        if(cases.equals("0")){
-            Bundle bundle = getIntent().getExtras();
-            master_list_id = bundle.getString("master_list_id");
-            user_id = bundle.getString("user_id");
-        }
-        if(cases.equals("1")){
-            master_list_id = prefs.getString("master_list_id", null);
-            user_id = prefs.getString("user_id",null);
-        }
-
-
-
-        Downloader d = new Downloader(this,addressTask,listView,2,user_id,master_list_id);
-
+        Downloader2 d = new Downloader2();
         d.execute();
 
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -69,8 +78,7 @@ public class TaskActivity extends AppCompatActivity {
                             boolean success = jsonResponse.getBoolean("success");
 
                             if(success){
-                                Downloader d2 = new Downloader(TaskActivity.this,addressTask,listView,2,user_id,master_list_id);
-
+                                Downloader2 d2 = new Downloader2();
                                 d2.execute();
 
                                 editText.setText("");
@@ -92,5 +100,282 @@ public class TaskActivity extends AppCompatActivity {
                 queue.add(createTaskRequest);
             }
         });
+    }
+
+    public class Downloader2 extends AsyncTask<Void, Integer,String>{
+
+        private ProgressDialog pd;
+
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(TaskActivity.this);
+            pd.setTitle("Donwloading");
+            pd.setMessage("Downloading...Please wait");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String data=null;
+            data = downloadData();
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pd.dismiss();
+
+            if(s != null){
+                Parser2 p = new Parser2(s);
+                p.execute();
+            }else{
+                Toast.makeText(TaskActivity.this,"Unable to download data",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private String downloadData(){
+            InputStream is = null;
+            String line = null;
+            String pass_data = null;
+
+
+
+            try {
+                pass_data = URLEncoder.encode("master_list_id","UTF-8")
+                        + "=" + URLEncoder.encode(master_list_id,"UTF-8");
+                pass_data += "&" + URLEncoder.encode("user_id", "UTF-8") + "=" + URLEncoder.encode(user_id,"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                URL url = new URL(addressTask);
+                HttpURLConnection con= (HttpURLConnection) url.openConnection();
+
+                con.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                if(pass_data != null){
+                    wr.write(pass_data);
+                    wr.flush();
+                }else{
+                    Toast.makeText(TaskActivity.this,"Error passing data to server", Toast.LENGTH_LONG).show();
+                }
+
+                is = new BufferedInputStream(con.getInputStream());
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                StringBuffer sb = new StringBuffer();
+
+                if(br != null){
+                    while((line=br.readLine()) != null){
+                        sb.append(line+"\n");
+                    }
+                }else{
+                    return null;
+                }
+
+                return sb.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                if(is != null){
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public class Parser2 extends AsyncTask<Void,Integer,Integer>
+    {
+        private String data;
+
+        private ArrayList<Model> modelArrayList;
+        private CustomAdapter2 customAdapter2;
+        private ArrayList<Model> list2 = new ArrayList<>();
+        private ProgressDialog pd;
+
+        public Parser2(String data){
+            this.data=data;
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(TaskActivity.this);
+            pd.setTitle("Parser");
+            pd.setMessage("Downloading...Please wait");
+            pd.show();
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            return parseData();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            pd.dismiss();
+            if(integer==1){
+                modelArrayList = list2;
+                customAdapter2 = new CustomAdapter2(modelArrayList);
+                listView.setAdapter(customAdapter2);
+            }else{
+                Toast.makeText(TaskActivity.this,"Unable to Parse",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private int parseData(){
+            try {
+                JSONArray ja = new JSONArray(data);
+                JSONObject jo = null;
+
+                list2.clear();
+
+                for(int i=0;i<ja.length();i++){
+                    jo = ja.getJSONObject(i);
+
+                    String task = jo.getString("task");
+                    Model model = new Model();
+                    model.setSelected(false);
+                    model.setTask(task);
+                    list2.add(model);
+
+                }
+                return 1;
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+    }
+
+    public class CustomAdapter2 extends BaseAdapter{
+        private  ArrayList<Model> modelArrayList;
+
+        public CustomAdapter2(ArrayList<Model> modelArrayList){
+            this.modelArrayList=modelArrayList;
+        }
+
+        public int getViewTypeCount(){
+            return getCount();
+        }
+
+        public int getItemViewType(int position){
+            return position;
+        }
+
+        @Override
+        public int getCount() {
+            return modelArrayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return modelArrayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final CustomAdapter2.ViewHolder2 holder;
+
+            if(convertView == null){
+                holder = new CustomAdapter2.ViewHolder2();
+                LayoutInflater inflater = (LayoutInflater) TaskActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.lv_item,null,true);
+
+                holder.checkBox = (CheckBox) convertView.findViewById(R.id.cb);
+                holder.task = (TextView) convertView.findViewById(R.id.task_textview);
+
+                convertView.setTag(holder);
+            }else{
+                holder = (CustomAdapter2.ViewHolder2) convertView.getTag();
+            }
+
+            holder.task.setText(modelArrayList.get(position).getTask());
+
+            holder.checkBox.setChecked(modelArrayList.get(position).getSelected());
+
+            holder.checkBox.setTag(R.integer.btnplusview, convertView);
+            holder.checkBox.setTag(position);
+            holder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View tempview = (View) holder.checkBox.getTag(R.integer.btnplusview);
+                    TextView tv = (TextView) tempview.findViewById(R.id.task_textview);
+                    Integer pos = (Integer) holder.checkBox.getTag();
+
+                    Toast.makeText(TaskActivity.this, "Checkbox "+pos+" clicked!",Toast.LENGTH_SHORT).show();
+
+                    if(modelArrayList.get(pos).getSelected()){
+
+                        // modelArrayList.get(pos).setSelected(false);
+
+                    /*
+                    */
+                    }else{
+
+                        modelArrayList.get(pos).setSelected(true);
+
+                        Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonResponse = new JSONObject(response);
+                                    boolean success = jsonResponse.getBoolean("success");
+
+                                    if(success){
+                                        Toast.makeText(TaskActivity.this, "THE OPERATION WAS SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(TaskActivity.this);
+                                        builder.setMessage("Registration failed");
+                                        builder.setNegativeButton("Retry",null);
+                                        builder.create();
+                                        builder.show();
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+                        String task = modelArrayList.get(pos).getTask().toString();
+                        modelArrayList.remove(modelArrayList.get(pos));
+                        notifyDataSetChanged();
+                        DeletListItemRequest d = new DeletListItemRequest(task,master_list_id,user_id,responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(TaskActivity.this);
+                        queue.add(d);
+                    }
+                }
+            });
+
+            return convertView;
+        }
+
+        private class ViewHolder2{
+            protected CheckBox checkBox;
+            private TextView task;
+        }
     }
 }
